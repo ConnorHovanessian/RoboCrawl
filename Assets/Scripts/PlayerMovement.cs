51db8d283a35b1cf08bd370f8bd9ecf4106e34e8
Connor <connor.hovanessian@gmail.com>
2020-01-20 18:18:42 -0800

    Sprint implementation, edge handling on jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,11 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public Transform groundCheck;
-    public float groundDistance = 0.4f;
+    public float groundDistance = 0.1f;
     public LayerMask groundMask; 
 
-    public float speed = 12f;
+    public float speed = 10f;
+    public float sprintModifier = 1.75f;
     public float gravity = -9.81f * 2;
     public float jumpHeight = 3f;
 
@@ -20,20 +21,41 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Groundcheck
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         if(isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f;
+            controller.slopeLimit = 45.0f;
+            velocity.y = -1.5f;
         }
+
         //Player Movement
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Vector3 move = transform.right * x + transform.forward * z;
+            controller.Move(move * speed * sprintModifier * Time.deltaTime);
+        }
+        else
+        {
+            Vector3 move = transform.right * x + transform.forward * z;
+            controller.Move(move * speed * Time.deltaTime);
+        }
+
+
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            controller.slopeLimit = 90.0f;
         }
+
+        if ((controller.collisionFlags & CollisionFlags.Above) != 0)
+        {
+            velocity.y = -2f;
+        }
+
         //Gravity
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
