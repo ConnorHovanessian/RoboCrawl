d197db7395ffe8e041ac4703329fb8a4be61fe66
Connor <connor.hovanessian@gmail.com>
2020-01-24 15:08:58 -0800

    added shooting physics, enemy health and death

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 50;
+
+    public void takeDamage(float amount)
+    {
+        health -= amount;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
